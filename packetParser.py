56b157106c1425105dc3396d4f3a312d50ffb68d
altlsa125 <sircuddlesIII@gmail.com>
2024-03-09 11:55:07 -0800

    added packet parser

--- packetParser.py
+++ packetParser.py
@@ -1,28 +1,4 @@
-import BankAPI.dns_bank_api as bank
-import NetworkAPI.dns_network_api as net
-import asyncio
 import re
-
-NET_ID = 100
-
-# async def process_transactions():
-#     # Get the transaction from the network
-#     transaction = await net.network_request(NET_ID)
-#
-#     # Parse the transaction to get the necessary details
-#     # This is a placeholder, replace it with actual parsing logic
-#     transaction_details = transaction.split(',')
-
-
-async def main():
-    print(bank.bank_status())
-    while True:
-        print(await net.network_check(NET_ID))
-        packet = await net.network_request()
-        await asyncio.sleep(5)  # pause for 5 seconds
-
-# Run the main function
-asyncio.run(main())
 
 def process_packet(packet):
     # Define regular expressions to match the desired patterns
